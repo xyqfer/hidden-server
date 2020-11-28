bfaa6e941f7deddb7ab02cebf32d8a9ef4ee5603
凉凉 <xyqfer@gmail.com>
2020-11-28 16:44:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,22 +3,41 @@ const request = require('request');
 const https = require('https');
 const { SocksProxyAgent } = require('socks-proxy-agent');
 const tr = require('tor-request');
+const proxy = require('http-proxy-middleware');
 
-const agent = new SocksProxyAgent('socks5h://127.0.0.1:9050');
+const agent = new SocksProxyAgent('socks://127.0.0.1:9050');
 
 const app = express();
 
-app.get('/', function(req, res) {
-    res.send('index');
-});
+app.use(
+  '/',
+  proxy({
+    target: 'https://venus.web.telegram.org', 
+    changeOrigin: true,
+    agent,
+    router: (req) => {
+      const host = req.headers['xxxx-origin'];
+      delete req.headers['xxxx-origin'];
+      
+      return host;
+    },
+    onError: (err, req, res) => {
+      console.error(err);
+    },
+  }),
+);
 
-app.get('/proxyimage', (req, resp) => {
-    const { url } = req.query;
+// app.get('/', function(req, res) {
+//     res.send('index');
+// });
+
+// app.get('/proxyimage', (req, resp) => {
+//     const { url } = req.query;
     
-    tr.request(url, function (err, res, body) {
-      resp.send(body);
-    });
-});
+//     tr.request(url, function (err, res, body) {
+//       resp.send(body);
+//     });
+// });
 
 const PORT = parseInt(process.env.PORT || 3000);
 app.listen(PORT, function() {
